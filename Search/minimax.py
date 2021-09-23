c810e3cc619640336e8ad650810d959b205089f6
filhed97 <filhed97@gmail.com>
2021-09-23 23:36:02 +0200

    implemented working repeated states checking, fixed inDistSquare() so it accounts for x-axis wrapping

--- Search/minimax.py
+++ Search/minimax.py
@@ -3,11 +3,16 @@ import random
 import math
 import time
 
+from statehash import StateHash
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
 
 class Minimax:
+
+    def __init__(self):
+        self.tt = StateHash()
+
     #Heuristic evaluation function v2.0, sums player score and sums up weighted distance to fish. 
     #Takes into account both overall score and how good the hooks are positioned for each player.
     #When comparing different states, if the score favors opponent identically, this heuristic will give a better heuristic score to the state with better hook position (i.e. less negative).
@@ -35,10 +40,19 @@ class Minimax:
 
     #Helper function to calculate inverse square distance between sets of coordinates.
     def invSquareDist(this, tupleA, tupleB):
+        #check equality
         if(tupleA == tupleB):
             return 0
         x1,y1 = tupleA
         x2,y2 = tupleB
+        #adjust coordinates to account for "horizontal roll". I.e. if x1 = 0 and x2 = 19, the distance is 1, not 19.
+        #if there's a distance greater or equal to 10 between the two points, they are closer to each other by going through the world border than trough the open field.
+        if x1+10<x2:
+            x2 = x2-20
+        else:
+            if x2+10<x1:
+                x1 = x1-20
+
         return 1/(math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2)))
 
     def minimaxAB(self, node, depth, alpha, beta, player):
@@ -48,7 +62,12 @@ class Minimax:
 
         children = node.compute_and_get_children()
         if (depth==0 or len(children)==0):
-            return self.heuristic(player,node.state)
+            #check transposition table if state has been visited
+            h = self.tt.get(node.state)
+            if h=="empty":
+                h = self.heuristic(player,node.state)
+                self.tt.add(node.state,h)
+            return h
 
         if player==0:           #player A
             v = -(math.inf)
