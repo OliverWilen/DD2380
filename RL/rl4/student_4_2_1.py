c3980f69b93c85dbb513826467e3525f4d8aee54
OliverWilen <owilen@kth.se>
2021-10-14 13:17:06 +0200

    RL5

--- RL/rl4/student_4_2_1.py
+++ RL/rl4/student_4_2_1.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # rewards: [golden_fish, jellyfish_1, jellyfish_2, ... , step]
-rewards = [10, -10, -10, 10, -10, 10, -10, 10, -10, 10, -10, 10, -10]
+rewards = [-100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, 0]
 
 # Q learning learning rate
-alpha = 0.1
+alpha = 0.5
 
 # Q learning discount rate
-gamma = 0.1
+gamma = 0.7
 
 # Epsilon initial
 epsilon_initial = 1
 
 # Epsilon final
-epsilon_final = 0.2
+epsilon_final = 0.1
 
 # Annealing timesteps
-annealing_timesteps = 1000
+annealing_timesteps = 400
 
 # threshold
-threshold = 1e-6
+threshold = 1e-4
